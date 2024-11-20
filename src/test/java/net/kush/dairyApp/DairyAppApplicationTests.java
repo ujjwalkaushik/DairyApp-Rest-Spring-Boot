a13c85b8efa1de382c9a3e1dc165a0e19dc538fd
kush <ujjwalkaushik24@gmail.com>
2024-11-20 20:15:49 +0530

    Spring boot Initial Setup

---
+++ src/test/java/net/kush/dairyApp/DairyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kush.dairyApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DairyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
