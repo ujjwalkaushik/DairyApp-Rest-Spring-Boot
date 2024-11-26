c34f29b224c21a8ba4550a52d369307f221d5a48
kush <ujjwalkaushik24@gmail.com>
2024-11-26 22:39:27 +0530

    Dbref & collection relation

---
+++ src/main/java/net/kush/dairyApp/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package net.kush.dairyApp.repository;
+
+import net.kush.dairyApp.entity.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+
+    User findByUserName(String username);
+
+}
