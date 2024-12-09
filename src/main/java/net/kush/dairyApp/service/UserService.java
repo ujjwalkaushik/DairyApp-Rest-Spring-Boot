72d6c2455a9d209172ffc826c53ae5e105b69c76
kush <ujjwalkaushik24@gmail.com>
2024-12-09 21:08:21 +0530

    Dairy Endpoints Security

--- src/main/java/net/kush/dairyApp/service/UserService.java
+++ src/main/java/net/kush/dairyApp/service/UserService.java
@@ -20,9 +20,13 @@ public class UserService {
 
     private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-    public void  saveEntry(User user) {
+    public void saveNewUser(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setRoles(Arrays.asList("USER"));
+        userRepository.save(user);
+    }
+
+    public void saveUser(User user) {
         userRepository.save(user);
     }
 
